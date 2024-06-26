40598ed8a7f3b6982fbd17791d5e7b779e0d18e3
waltertaya <waltermittybecker5@gmail.com>
2024-06-26 23:35:22 +0300

    Updated: Task 4 -> output fail

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -9,23 +9,28 @@ class MRUCache(BaseCaching):
     """ MRUCache defines a MRU caching system """
 
     def __init__(self):
-        """ Initialize the class """
+        """Initializes the cache.
+        """
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Add an item in the cache """
-        if key and item:
-            if key in self.cache_data:
-                self.cache_data.move_to_end(key)
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", mru_key)
             self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                discarded_key, _ = self.cache_data.popitem(last=True)
-                print("DISCARD:", discarded_key)
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key """
-        if key in self.cache_data:
-            self.cache_data.move_to_end(key)
-            return self.cache_data[key]
-        return None
+        """Retrieves an item by key.
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
