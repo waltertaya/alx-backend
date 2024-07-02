8014afb903328a23791edd435dc9adef66cb2fb5
waltertaya <waltermittybecker5@gmail.com>
2024-07-02 19:24:42 +0300

    Fixed: 🔧Requirement fail ✅✅ Task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,6 +20,7 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
+    ''' get locale that matches the user's '''
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
