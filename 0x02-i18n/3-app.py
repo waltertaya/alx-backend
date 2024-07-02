f3b0b1612968c1481cec9103b5353ea0f963f96d
waltertaya <waltermittybecker5@gmail.com>
2024-07-02 20:06:06 +0300

    Fixed: 🔧Requirement fail ✅✅ Task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,35 +1,36 @@
 #!/usr/bin/env python3
 ''' 2-app.py file '''
-
+from flask_babel import Babel
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
-
-app = Flask(__name__)
 
 
 class Config:
-    ''' configurations for the app '''
+    """Represents a Flask Babel configuration.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
-    ''' get locale that matches the user's '''
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+    """
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
-def index():
-    ''' Render index template '''
-    return render_template('2-index.html')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
-    ''' Running the the flask server '''
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
