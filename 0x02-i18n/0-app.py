326e8b3c159d1d25439c89e69e8e36eeb27d29e0
waltertaya <waltermittybecker5@gmail.com>
2024-07-02 17:45:37 +0300

    Completed: Task 0 ✅✅

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+''' 0-app.py file '''
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    ''' Render index template '''
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    ''' Running the the flask server '''
+    app.run(debug=True)
