4d7a043ac9045805af7eb2ee65ddbbbd48c22145
waltertaya <waltermittybecker5@gmail.com>
2024-07-02 18:23:54 +0300

    Cpmpleted: Task 1 ✅✅

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+''' 1-app.py file '''
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    ''' Render index template '''
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    ''' Running the the flask server '''
+    app.run()
